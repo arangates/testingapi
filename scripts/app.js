956455a6e06a683a7d1ef401ba97672c272b8cdf
arangates <arangates@gmail.com>
2016-09-08 15:10:37 +0530

    first commit

---
+++ scripts/app.js
@@ -0,0 +1,66 @@
+
+(function(document) {
+  'use strict';
+
+  // Grab a reference to our auto-binding template
+  // and give it some initial binding values
+  // Learn more about auto-binding templates at http://goo.gl/Dx1u2g
+  var app = document.querySelector('#app');
+
+  // Sets app default base URL
+  app.baseUrl = '/';
+  if (window.location.port === '') {  // if production
+    // Uncomment app.baseURL below and
+    // set app.baseURL to '/your-pathname/' if running from folder in production
+    // app.baseUrl = '/polymer-starter-kit/';
+  }
+
+   app.displayInstalledToast = function() {
+    // Check to make sure caching is actually enabled—it won't be in the dev environment.
+    if (!Polymer.dom(document).querySelector('platinum-sw-cache').disabled) {
+      Polymer.dom(document).querySelector('#caching-complete').show();
+    }
+  };
+
+  // Listen for template bound event to know when bindings
+  // have resolved and content has been stamped to the page
+  app.addEventListener('dom-change', function() {
+
+    // console.log('Our app is ready to rock!');
+   if (localStorage.getItem("username") == null || localStorage.getItem("username") == 0) {
+    console.log('no username');
+  animated.open();
+  
+setTimeout(function(){ animated.refit(); }, 300);
+geoFindMe();
+
+}
+
+
+  else {
+    console.log(localStorage.getItem("username"));
+  }
+
+  });
+
+  // See https://github.com/Polymer/polymer/issues/1381
+  window.addEventListener('WebComponentsReady', function() {
+    // imports are loaded and elements have been registered
+  });
+
+  // Main area's paper-scroll-header-panel custom condensing transformation of
+  // the appName in the middle-container and the bottom title in the bottom-container.
+  // The appName is moved to top and shrunk on condensing. The bottom sub title
+  // is shrunk to nothing on condensing.
+ 
+
+  // Scroll page to top and expand header
+  // app.scrollPageToTop = function() {
+  //   app.$.headerPanelMain.scrollToTop(true);
+  // };
+
+  // app.closeDrawer = function() {
+  //   app.$.paperDrawerPanel.closeDrawer();
+  // };
+
+})(document);
